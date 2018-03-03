68fa78e7fbcadc12bcb9a721291918bb0bfd8715
Chris Henry <cdhenry@gmail.com>
2018-03-03 21:40:12 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sally", 3, "F", "black", "easy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Garmin", 14, "M", "brown", "difficult", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Peter", 21, "M", "brown", "easy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rascal", 1, "M", "white", "slow to warm up", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Billy", 25, "M", "black", "difficult", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Anita", 11, "F", "red", "slow to warm up", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Phyllis", 9, "F", "white", "easy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ();
+
+                                
